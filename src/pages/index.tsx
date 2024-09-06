ca234adceb548830054ba8c30d37cfa3732342dc
Aishwarya Apsangi <155040036+AishwaryaApsangi@users.noreply.github.com>
2024-09-06 08:44:06 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,8 +21,6 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
   type CarouselApi,
 } from "@/components/ui/carousel";
 import VanillaTilt from "vanilla-tilt";
@@ -71,7 +69,7 @@ const services = [
   {
     service: "Computer Programming Tutor at CIS Sandbox Bentley University",
     description:
-      "Provide in-depth explanations of programming principles, debug students' code, and offer practical examples to enhance their problem-solving skills.",
+      "Provide in-depth explanations of programming principles, debug students&apos; code, and offer practical examples to enhance their problem-solving skills.",
     icon: Code2,
   },
   {
@@ -83,23 +81,23 @@ const services = [
   {
     service: "Indie music and coffee lover",
     description:
-      "I love spending my free time listening to indie music from artists like Beabadoobee, Steve Lacy, and The Smiths, and enjoying a good books, and exploring new cafes!",
+      "I love spending my free time listening to indie music from artists like Beabadoobee, Steve Lacy, and The Smiths, and enjoying a good book, and exploring new cafes!",
     icon: SearchCheck,
   },
   {
     service: "Technology Chair, NSLS - National Society of Leadership and Success",
     description:
-      "Led technology initiatives and digital strategies to support the organization's member engagement",
+      "Led technology initiatives and digital strategies to support the organization&apos;s member engagement",
     icon: MonitorSmartphone,
   },
   {
-    service: "Bentley Women's Leadership Program",
+    service: "Bentley Women&apos;s Leadership Program",
     description:
       "Leadership development workshops/mentoring sessions focused on empowering women on-campus.",
     icon: Eye,
   },
   {
-    service: "Fall '23 Apparel Chair for Alpha Psi Omega",
+    service: "Fall &apos;23 Apparel Chair for Alpha Psi Omega",
     description:
       "Managed apparel design, including budgeting and coordinating orders for the International Theatre Fraternity.",
     icon: Eye,
@@ -284,9 +282,9 @@ export default function Home() {
                 Python, Java, and JavaScript
               </Link>{" "}
               leveraging frameworks such as Flask, GraphQL, and Next.js. Since 2021, 
-              I've contributed to various projects ranging from AI/ML model development 
+              I&apos;ve contributed to various projects ranging from AI/ML model development 
               to interactive dashboards for data visualization. My experience includes working with organizations 
-              like Deloitte, MITRE, and NetAtlantic, where I've played a key role in 
+              like Deloitte, MITRE, and NetAtlantic, where I&apos;ve played a key role in 
               developing robust web applications, optimizing data communication, and driving technology adoption, all while
               efficiently collaborating with cross-functional teams.
             </h2>
@@ -309,99 +307,98 @@ export default function Home() {
         </section>
 
         {/* Projects */}
-<section id="projects" data-scroll-section>
-  {/* Gradient */}
-  <div className="relative isolate -z-10">
-    <div
-      className="absolute inset-x-0 -top-40 transform-gpu overflow-hidden blur-[100px] sm:-top-80 lg:-top-60"
-      aria-hidden="true"
-    >
-      <div
-        className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-primary via-primary to-secondary opacity-10 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-        style={{
-          clipPath:
-            "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-        }}
-      />
-    </div>
-  </div>
-  <div data-scroll data-scroll-speed=".4" className="my-64">
-    <span className="text-gradient clash-grotesk text-sm font-semibold tracking-tighter">
-      ✨ Projects
-    </span>
-    <h2 className="mt-3 text-4xl font-semibold tracking-tight tracking-tighter xl:text-6xl">
-      Streamlined digital experiences.
-    </h2>
-    <p className="mt-1.5 text-base tracking-tight text-muted-foreground xl:text-lg">
-      I&apos;ve worked on a variety of projects, from small websites to
-      large-scale web applications. Here are some of my favorites:
-    </p>
+        <section id="projects" data-scroll-section>
+          {/* Gradient */}
+          <div className="relative isolate -z-10">
+            <div
+              className="absolute inset-x-0 -top-40 transform-gpu overflow-hidden blur-[100px] sm:-top-80 lg:-top-60"
+              aria-hidden="true"
+            >
+              <div
+                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-primary via-primary to-secondary opacity-10 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+              />
+            </div>
+          </div>
+          <div data-scroll data-scroll-speed=".4" className="my-64">
+            <span className="text-gradient clash-grotesk text-sm font-semibold tracking-tighter">
+              ✨ Projects
+            </span>
+            <h2 className="mt-3 text-4xl font-semibold tracking-tight xl:text-6xl">
+              Streamlined digital experiences.
+            </h2>
+            <p className="mt-1.5 text-base tracking-tight text-muted-foreground xl:text-lg">
+              I&apos;ve worked on a variety of projects, from small websites to
+              large-scale web applications. Here are some of my favorites:
+            </p>
 
-    {/* Carousel */}
-    <div className="mt-14 relative">
-      <Carousel setApi={setCarouselApi} className="w-full">
-        <CarouselContent>
-          {projects.map((project) => (
-            <CarouselItem key={project.title} className="md:basis-1/2">
-              <Card id="tilt">
-                <CardHeader className="p-0">
-                  <Link href={project.href} target="_blank" passHref>
-                    {project.image.endsWith(".webm") ? (
-                      <video
-                        src={project.image}
-                        autoPlay
-                        loop
-                        muted
-                        className="aspect-video h-full w-full rounded-t-md bg-primary object-cover"
-                      />
-                    ) : (
-                      <Image
-                        src={project.image}
-                        alt={project.title}
-                        width={600}
-                        height={300}
-                        quality={100}
-                        className="aspect-video h-full w-full rounded-t-md bg-primary object-cover"
-                      />
-                    )}
-                  </Link>
-                </CardHeader>
-                <CardContent className="absolute bottom-0 w-full bg-background/50 backdrop-blur">
-                  <CardTitle className="border-t border-white/5 p-4 text-base font-normal tracking-tighter">
-                    {project.description}
-                  </CardTitle>
-                </CardContent>
-              </Card>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
+            {/* Carousel */}
+            <div className="mt-14 relative">
+              <Carousel setApi={setCarouselApi} className="w-full">
+                <CarouselContent>
+                  {projects.map((project) => (
+                    <CarouselItem key={project.title} className="md:basis-1/2">
+                      <Card id="tilt">
+                        <CardHeader className="p-0">
+                          <Link href={project.href} target="_blank" passHref>
+                            {project.image.endsWith(".webm") ? (
+                              <video
+                                src={project.image}
+                                autoPlay
+                                loop
+                                muted
+                                className="aspect-video h-full w-full rounded-t-md bg-primary object-cover"
+                              />
+                            ) : (
+                              <Image
+                                src={project.image}
+                                alt={project.title}
+                                width={600}
+                                height={300}
+                                quality={100}
+                                className="aspect-video h-full w-full rounded-t-md bg-primary object-cover"
+                              />
+                            )}
+                          </Link>
+                        </CardHeader>
+                        <CardContent className="absolute bottom-0 w-full bg-background/50 backdrop-blur">
+                          <CardTitle className="border-t border-white/5 p-4 text-base font-normal tracking-tighter">
+                            {project.description}
+                          </CardTitle>
+                        </CardContent>
+                      </Card>
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
 
-        {/* Left Navigation Button */}
-      <button
-        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white/30 p-2 rounded-full hover:bg-white/50"
-        onClick={() => carouselApi?.scrollPrev()} 
-      >
-        <ChevronLeft className="h-5 w-5 text-primary" />
-      </button>
+                {/* Left Navigation Button */}
+                <button
+                  className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white/30 p-2 rounded-full hover:bg-white/50"
+                  onClick={() => carouselApi?.scrollPrev()} 
+                >
+                  <ChevronLeft className="h-5 w-5 text-primary" />
+                </button>
 
-        {/* Navigation Button - Only Right Side */}
-        <button
-          className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white/30 p-2 rounded-full hover:bg-white/50"
-          onClick={() => carouselApi?.scrollNext()}
-        >
-          <ChevronRight className="h-5 w-5 text-primary" />
-        </button>
-      </Carousel>
-      <div className="py-2 text-center text-sm text-muted-foreground">
-        <span className="font-semibold">
-          {current} / {count}
-        </span>{" "}
-        projects
-      </div>
-    </div>
-  </div>
-</section>
-
+                {/* Right Navigation Button */}
+                <button
+                  className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white/30 p-2 rounded-full hover:bg-white/50"
+                  onClick={() => carouselApi?.scrollNext()}
+                >
+                  <ChevronRight className="h-5 w-5 text-primary" />
+                </button>
+              </Carousel>
+              <div className="py-2 text-center text-sm text-muted-foreground">
+                <span className="font-semibold">
+                  {current} / {count}
+                </span>{" "}
+                projects
+              </div>
+            </div>
+          </div>
+        </section>
 
         {/* Services */}
         <section id="services" data-scroll-section>
@@ -412,66 +409,60 @@ export default function Home() {
             className="my-24 flex flex-col justify-start space-y-10"
           >
             <div className="w-full my-6">
-  {/* Start Services Section */}
-  <div className="w-full my-6">
-  {/* Start Services Section */}
-  <motion.div
-    initial={{ opacity: 0, y: -10 }}
-    whileInView={{ opacity: 1, x: 0 }}
-    transition={{
-      duration: 1,
-      staggerChildren: 0.5,
-    }}
-    viewport={{ once: true }}
-    className="grid w-full items-start gap-4 md:grid-cols-2 xl:grid-cols-3"
-  >
-    {/* Image Section */}
-    <div className="flex flex-col py-6 xl:p-6">
-      <h2 className="text-4xl font-medium tracking-tight">
-        Get to Know Me Better
-        <br />
-        <span className="text-gradient clash-grotesk tracking-normal">
-          Campus Involvements & Interests
-        </span>
-      </h2>
-      <p className="mt-2 tracking-tighter text-secondary-foreground">
-        From supporting my peers in tech to exploring my love for indie music, reading, and coffee, 
-        here's what I'm all about.
-      </p>
-      {/* Image Here */}
-      <div className="my-6">
-        <Image
-          src="/assets/image.png"
-          alt="Description of the image"
-          width={500}
-          height={400}
-          className="rounded-md shadow-md"
-        />
-      </div>
-    </div>
+              {/* Start Services Section */}
+              <motion.div
+                initial={{ opacity: 0, y: -10 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{
+                  duration: 1,
+                  staggerChildren: 0.5,
+                }}
+                viewport={{ once: true }}
+                className="grid w-full items-start gap-4 md:grid-cols-2 xl:grid-cols-3"
+              >
+                {/* Image Section */}
+                <div className="flex flex-col py-6 xl:p-6">
+                  <h2 className="text-4xl font-medium tracking-tight">
+                    Get to Know Me Better
+                    <br />
+                    <span className="text-gradient clash-grotesk tracking-normal">
+                      Campus Involvements & Interests
+                    </span>
+                  </h2>
+                  <p className="mt-2 tracking-tighter text-secondary-foreground">
+                    From supporting my peers in tech to exploring my love for indie music, reading, and coffee, 
+                    here&apos;s what I&apos;m all about.
+                  </p>
+                  {/* Image Here */}
+                  <div className="my-6">
+                    <Image
+                      src="/assets/image.png"
+                      alt="Description of the image"
+                      width={500}
+                      height={400}
+                      className="rounded-md shadow-md"
+                    />
+                  </div>
+                </div>
 
-    {/* Services Items */}
-    {services.map((service) => (
-      <div
-        key={service.service}
-        className="flex flex-col h-full items-start rounded-md bg-white/5 p-14 shadow-md backdrop-blur transition duration-300 hover:-translate-y-0.5 hover:bg-white/10 hover:shadow-md"
-      >
-        <service.icon className="my-6 text-primary" size={20} />
-        <span className="text-lg tracking-tight text-foreground">
-          {service.service}
-        </span>
-        <span className="mt-2 tracking-tighter text-muted-foreground">
-          {service.description}
-        </span>
-      </div>
-    ))}
-  </motion.div>
-</div>
-
-</div>
-
+                {/* Services Items */}
+                {services.map((service) => (
+                  <div
+                    key={service.service}
+                    className="flex flex-col h-full items-start rounded-md bg-white/5 p-14 shadow-md backdrop-blur transition duration-300 hover:-translate-y-0.5 hover:bg-white/10 hover:shadow-md"
+                  >
+                    <service.icon className="my-6 text-primary" size={20} />
+                    <span className="text-lg tracking-tight text-foreground">
+                      {service.service}
+                    </span>
+                    <span className="mt-2 tracking-tighter text-muted-foreground">
+                      {service.description}
+                    </span>
+                  </div>
+                ))}
+              </motion.div>
+            </div>
           </div>
-          
         </section>
 
         {/* Contact */}
@@ -482,15 +473,15 @@ export default function Home() {
             data-scroll-position="top"
             className="flex flex-col items-center justify-center rounded-lg bg-gradient-to-br from-primary/[6.5%] to-white/5 px-8 py-16 text-center xl:py-24"
           >
-             {/* Social Media Links */}
+            {/* Social Media Links */}
             <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
               {/* GitHub Link */}
               <a href="https://github.com/AishwaryaApsangi" target="_blank" title="Link to Github Profile" style={{ margin: '0 10px' }}>
-                <img src="/assets/github.svg" alt="Github" style={{ width: '32px', height: '32px' }} />
+                <Image src="/assets/github.svg" alt="Github" width={32} height={32} />
               </a>
               {/* LinkedIn Link */}
               <a href="https://www.linkedin.com/in/aishwaryaapsangi/" target="_blank" title="Link to Linkedin Profile" style={{ margin: '0 10px' }}>
-                <img src="/assets/linkedin.svg" alt="Linkedin" style={{ width: '32px', height: '32px' }} />
+                <Image src="/assets/linkedin.svg" alt="Linkedin" width={32} height={32} />
               </a>
             </div>
 
@@ -571,3 +562,4 @@ function Gradient() {
     </>
   );
 }
+
